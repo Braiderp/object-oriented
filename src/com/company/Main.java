5d01241d3a047e37aa066692a6c0e4a3897e452e
en3m3 <mof20002@byui.edu>
2021-01-28 20:35:53 -0700

    Added my hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello, my name is Braiden Parkinson");
         System.out.println("Hi Braiden, my name is Erik");
+
     }
 }
